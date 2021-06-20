fbda8f2d82da6d2c6f4d406bdcb5dbd43da10df4
mashru020 <mashrurrahman16@gmail.com>
2021-06-20 13:05:58 +0600

    1st PSD to HTML Practice 1(BO)

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	$('.gallery').each(function() { // the containers for all your galleries
+    $(this).magnificPopup({
+        delegate: 'a', // the selector for gallery item
+        type: 'image',
+        gallery: {
+          enabled:true
+        }
+    });
+});
+$('.category-filter .category-button').categoryFilter();
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36251023-1']);
+  _gaq.push(['_setDomainName', 'jqueryscript.net']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  var menuBtn = document.querySelector('.nav-icon');
+		var menuOpen = false;
+		menuBtn.addEventListener('click', function(){ 
+			if(!menuOpen){
+		 		menuBtn.classList.add("open");
+		 		menuOpen= true;
+			}
+		 	else{
+		 		menuBtn.classList.remove("open");
+		 		menuOpen = false;
+		 	}
+		 });
+$('#services').on('slide.bs.carousel', function (e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;   
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+});
